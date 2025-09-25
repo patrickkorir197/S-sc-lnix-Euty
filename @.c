b14e0e0570399362e0e8e431dfc2b8b3dea3194e
Patrick <patrickkorir197@gmail.com>
2025-09-25 12:04:21 +0300

    Add files via upload

---
+++ @.c
@@ -0,0 +1,24 @@
+/*
+Name:paKipkemoi Korir
+Reg:CT101/G/25878/25
+*/
+
+#include<stdio.h>
+int main() {
+printf("Hello, World \n");
+printf("sasa\n");
+printf("1 mathematics for science\n");
+printf("2 physics\n");
+printf("3 analogue electronics\n");
+printf("4 communication skills\n");
+printf("5 introduction to programming and problem solving\n");
+printf("6 fundamental of ict\n");
+printf("7 computer system architectur\n");
+printf("8 discreet mathematics\n");
+printf(" this is all the units am taking in my first semister");
+}
+
+
+
+
+    
