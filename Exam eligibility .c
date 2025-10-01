7eeec364fb0208465602f8ad0db09d9c4b67df6c
Patrick <patrickkorir197@gmail.com>
2025-10-01 19:35:35 +0300

    Add files via upload

---
+++ Exam eligibility .c
@@ -0,0 +1,23 @@
+/*
+Name: Patrick Kipkemoi Korir 
+Registration no: CT101/G/25878/25
+Description: Program exam eligibility  
+Description:exam eligibility 
+*/
+
+#include<stdio.h>
+int main()
+{
+float attendance;
+float average_marks;
+
+printf("Enter your attendance");
+scanf("%d", &attendance);
+
+printf("average_marks");
+scanf("%f", &average_marks);
+if(attendance<=75 &&average_marks>=40)
+{printf("you are eligible for exam.\n");} else
+{printf("you are not eligible for exam.\n");}
+return 0;
+}
