8aee327425b83323544b039fbd53463f338f7b81
Patrick <patrickkorir197@gmail.com>
2025-10-21 23:39:22 +0300

    Add files via upload

---
+++ calculate_Fare.C
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+float calculateFare(float distance);
+int main ()
+{
+float distance;
+
+printf("Enter distance travelled(in km)");
+
+scanf("Total Fare=Ksh.%2f\n");
+
+printf("assume the fare rate is<=50");
+scanf("%f.<=50");
+
+return 0;
+
+}
+
+    
